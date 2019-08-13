7805dce9a2fa509437d2ea4ed92f9bdd691e4809
Ravindu Kariyapperuma <ravindu.kariyapperuma@gmail.com>
2019-08-13 13:33:32 +0530

    first commit

---
+++ payment.js
@@ -0,0 +1,36 @@
+
+function validateMyForm()
+{
+var x=document.forms["paymentform"]["cardno"].value;
+var y=document.forms["paymentform"]["exday"].value;
+var z=document.forms["paymentform"]["scode"].value;
+if (x==null||x==""){
+alert("Card Number must be filled out ");
+return false;
+}
+else if (document.paymentform.cardno.value.length<16)
+{alert("Your Card Number must be at lest 10 characters long");
+return false;
+}
+if (y==null||y==""){
+alert("Expiration date must be filled out ");
+return false;
+}
+if (z==null||z==""){
+alert("Security Code must be filled out ");
+return false;
+}
+else{
+getconfirmation();
+}
+}
+function getconfirmation(){
+var retVal=confirm("Do you want to continue ? ");
+if(retVal==true){
+//document.write("User wants to continue !");
+}
+else{
+window.open('home.html')
+return false;
+}
+}
